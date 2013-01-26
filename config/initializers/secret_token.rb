8bb3c0db97ac9ee50d448e58d7fbf7fa24c2ca1e
johnny <j.e.miyake@gmail.com>
2013-01-26 15:07:58 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RadiheyRails::Application.config.secret_token = 'a504ca6783d4da0130d1c5d44d62716f2e2c5c129a546cbac2d19bd6e3361d1140ba7113c64298680889439b2bf690bd697cfb1e2600aa2a0597ec954c6ead0c'
