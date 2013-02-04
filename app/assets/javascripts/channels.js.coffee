8930b983a03f626b26546d7392d08da8f4713bed
johnny <j.e.miyake@gmail.com>
2013-02-04 23:14:38 +0900

    Ustreamの動画情報をURLをもとに取得するようにした

---
+++ app/assets/javascripts/channels.js.coffee
@@ -0,0 +1,41 @@
+window.is_valid_ust_url = (url) ->
+  r = url.match(/^http:\/\/www\.ustream\.tv\/channel\/([^\/]+).*/)
+  if (r && r[1])
+    true
+  else
+    false
+
+window.change_ac_ust_submit = (show_warning=true) ->
+  url = $('#ustream_channel_url').val()
+  if window.is_valid_ust_url(url)
+    $('.ac_ust_submit').removeClass('disabled')
+    $('.invalid_url').removeClass('show')
+  else
+    $('.ac_ust_submit').addClass('disabled')
+    $('.invalid_url').addClass('show') if show_warning
+
+$ ->
+  window.change_ac_ust_submit(false)
+
+  $('#ustream_channel_url').bind 'paste keyup', ->
+    window.change_ac_ust_submit()
+
+  $('.ac_ust_submit').click ->
+    url = $('#ustream_channel_url').val()
+    r = url.match(/^http:\/\/www\.ustream\.tv\/channel\/([^\/]+).*/)
+    if (r)
+      ust_uid = r[1]
+      $.ajax {
+        url: "http://api.ustream.tv/json/channel/"+ust_uid+"/getInfo",
+        dataType: 'jsonp',
+        jsonp: 'callback',
+        timeout: 2000,
+        success: (data, status) ->
+          if $('#ustream_channel_title').val().length + $('#ustream_channel_description').val().length > 0
+            return unless confirm('現在入力されている [チャンネルのタイトル] と [チャンネルの説明] が上書かれますが、よろしいですか？')
+
+          $('#ustream_channel_title').val("#{data.title}")
+          $('#ustream_channel_description').val("#{data.description}")
+          $('#ustream_channel_url').val("#{data.url}")
+          $('#preview').val("#{data.url}")
+      }
--- app/models/ustream_channel.rb
+++ app/models/ustream_channel.rb
@@ -1,25 +1,39 @@
-class ValidUstreamUidValidator < ActiveModel::EachValidator
+class ValidUstreamUrlValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
+    err_flag = false
+    unless /^http:\/\/www\.ustream\.tv\/channel\/([^\/]+).*/ =~ record.url
+      err_flag = true
+    end
     unless record.embed_tag
-      record.errors[attribute] << I18n.t('custom_validation.invalid_ust_uid')
+      err_flag = true
+    end
+    if err_flag
+      record.errors[attribute] << I18n.t('custom_validation.invalid_url')
     end
   end
 end
 
 class UstreamChannel < Channel
-  field :ust_uid, type: String;
-  validates :ust_uid, presence: true
-  validates :ust_uid, valid_ustream_uid: true
+  field :url, type: String;
+  validates :url, presence: true
+  validates :url, valid_ustream_url: true
 
   def embed_tag
     width = 480
     height = 300
-    if json = open("http://api.ustream.tv/json/channel/#{self.ust_uid}/getCustomEmbedTag?key=#{Ustream::KEY}&params=autoplay:false;mute:false;height:#{height};width:#{width}").read
-      begin
-        result_obj = JSON.parse(json)
-        return result_obj['results']
-      rescue Exception => e
-      end
+
+    /^http:\/\/www\.ustream\.tv\/channel\/([^\/]+).*/ =~ self.url
+    ust_uid = $1
+
+    unless json = open("http://api.ustream.tv/json/channel/#{ust_uid}/getCustomEmbedTag?key=#{Ustream::KEY}&params=autoplay:false;mute:false;height:#{height};width:#{width}").read
+      return nil
+    end
+
+    begin
+      result_obj = JSON.parse(json)
+      return result_obj['results']
+    rescue Exception => e
+      return nil
     end
   end
 
