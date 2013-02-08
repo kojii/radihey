ef24a3817cf64840b286c65afb2c3270d1952160
johnny <j.e.miyake@gmail.com>
2013-02-08 11:17:36 +0900

    seedを更新

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,20 @@
 # coding:utf-8
 
-#Station.delete_all
-#Station.create({code: 'tbs-radio',      name: 'TBSラジオ'})
-#Station.create({code: 'bunka-housou',   name: '文化放送'})
-#Station.create({code: 'nippon-housou',  name: 'ニッポン放送'})
-#Station.create({code: 'radio-nikkei',   name: 'ラジオNIKKEI'})
-#Station.create({code: 'dai1-radio',     name: '第1 ラジオ'})
-#Station.create({code: 'nikkei-dai2',    name: 'NIKKEI第2'})
-#Station.create({code: 'inter-fm',       name: 'InterFM'})
-#Station.create({code: 'tokyo-fm',       name: 'TOKYO FM'})
-#Station.create({code: 'j-wave',         name: 'J-WAVE'})
-#Station.create({code: 'radio-nippon',   name: 'ラジオ日本'})
-#Station.create({code: 'bayfm78',        name: 'bayfm78'})
-#Station.create({code: 'nack5',          name: 'NACK5'})
-#Station.create({code: 'fm-yokohama',    name: 'ＦＭヨコハマ'})
-#Station.create({code: 'housou-daigaku', name: '放送大学'})
+Station.delete_all
+Station.create({code: 'tbs-radio',      name: 'TBSラジオ'})
+Station.create({code: 'bunka-housou',   name: '文化放送'})
+Station.create({code: 'nippon-housou',  name: 'ニッポン放送'})
+Station.create({code: 'radio-nikkei',   name: 'ラジオNIKKEI'})
+Station.create({code: 'dai1-radio',     name: '第1 ラジオ'})
+Station.create({code: 'nikkei-dai2',    name: 'NIKKEI第2'})
+Station.create({code: 'inter-fm',       name: 'InterFM'})
+Station.create({code: 'tokyo-fm',       name: 'TOKYO FM'})
+Station.create({code: 'j-wave',         name: 'J-WAVE'})
+Station.create({code: 'radio-nippon',   name: 'ラジオ日本'})
+Station.create({code: 'bayfm78',        name: 'bayfm78'})
+Station.create({code: 'nack5',          name: 'NACK5'})
+Station.create({code: 'fm-yokohama',    name: 'ＦＭヨコハマ'})
+Station.create({code: 'housou-daigaku', name: '放送大学'})
 
 ButtonImage.delete_all
 bi_hee = ButtonImage.create({filename: 'button_hee.png'})
@@ -25,6 +25,7 @@ bi_warai = ButtonImage.create({filename: 'button_warai.png'})
 bi_majide = ButtonImage.create({filename: 'button_majide.png'})
 
 DefaultButtonSe.delete_all
+ButtonSe.delete_all
 dbs_hee = DefaultButtonSe.create({name: 'へぇ〜', audio: 'se_hee'})
 dbs_hai = DefaultButtonSe.create({name: 'はい', audio: 'se_hai'})
 dbs_iine = DefaultButtonSe.create({name: 'いいね！', audio: 'se_iine'})
