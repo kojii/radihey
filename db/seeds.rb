12b971b4c8614b53cd347f80dbac9235b9336d77
johnny <j.e.miyake@gmail.com>
2013-02-08 15:43:21 +0900

    seedを変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 # coding:utf-8
 
-#Station.delete_all
+Station.delete_all
 Station.create({code: 'tbs-radio',      name: 'TBSラジオ'})
 Station.create({code: 'bunka-housou',   name: '文化放送'})
 Station.create({code: 'nippon-housou',  name: 'ニッポン放送'})
@@ -16,7 +16,7 @@ Station.create({code: 'nack5',          name: 'NACK5'})
 Station.create({code: 'fm-yokohama',    name: 'ＦＭヨコハマ'})
 Station.create({code: 'housou-daigaku', name: '放送大学'})
 
-#ButtonImage.delete_all
+ButtonImage.delete_all
 bi_hee = ButtonImage.create({filename: 'button_hee.png'})
 bi_hai = ButtonImage.create({filename: 'button_hai.png'})
 bi_iine = ButtonImage.create({filename: 'button_iine.png'})
@@ -24,7 +24,7 @@ bi_ee = ButtonImage.create({filename: 'button_ee.png'})
 bi_warai = ButtonImage.create({filename: 'button_warai.png'})
 bi_majide = ButtonImage.create({filename: 'button_majide.png'})
 
-#ButtonSe.delete_all
+ButtonSe.delete_all
 dbs_hee = DefaultButtonSe.create({name: 'へぇ〜', audio: 'se_hee'})
 dbs_hai = DefaultButtonSe.create({name: 'はい', audio: 'se_hai'})
 dbs_iine = DefaultButtonSe.create({name: 'いいね！', audio: 'se_iine'})
@@ -32,7 +32,7 @@ dbs_ee = DefaultButtonSe.create({name: 'え〜', audio: 'se_ee'})
 dbs_warai = DefaultButtonSe.create({name: '笑', audio: 'se_warai'})
 dbs_majide = DefaultButtonSe.create({name: 'マジで！', audio: 'se_majide'})
 
-#Button.delete_all
+Button.delete_all
 DefaultButton.create({name: 'へぇ〜', button_label: 'へぇ〜', button_se_id: dbs_hee.id, button_image_id: bi_hee.id})
 DefaultButton.create({name: 'はい', button_label: 'はい', button_se_id: dbs_hai.id, button_image_id: bi_hai.id})
 DefaultButton.create({name: 'いいね！', button_label: 'いいね！', button_se_id: dbs_iine.id, button_image_id: bi_iine.id})
