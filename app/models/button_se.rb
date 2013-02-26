758686a1ac3a807610eff89b6e71464555db7e18
johnny <j.e.miyake@gmail.com>
2013-02-26 22:40:52 +0900

    ボタンのデフォルト効果音もpaperclipでアップロードするようにした

--- app/models/button_se.rb
+++ app/models/button_se.rb
@@ -1,10 +1,21 @@
 class ButtonSe
   include Mongoid::Document
   include Mongoid::Timestamps
+  include Mongoid::Paperclip
 
-  field :name,  type: String
-  field :audio, type: String, default: 'button_hee'
   has_many :buttons
 
+  field :name,  type: String
+  has_mongoid_attached_file :audio, {
+    :path           => '/button_se/:attachment/:id/:filename',
+    :storage        => :s3,
+    :url            => ':s3_alias_url',
+    :s3_host_alias  => 'radiheyrails.s3.amazonaws.com',
+    :s3_credentials => File.join(Rails.root, 'config', 's3.yml')
+  }
+
   validates :name, presence: true
+  validates_attachment :audio, presence: true,
+    content_type: {content_type: ['audio/wav','audio/x-wav']},
+    size: {in: 0..1.megabytes}
 end
