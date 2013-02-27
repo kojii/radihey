6f93994da0ad4c6b6860b4031a4d8ca703d5fdd5
johnny <j.e.miyake@gmail.com>
2013-02-27 13:35:36 +0900

    環境によってs3の接続先を変更するようにした

--- app/models/button_se.rb
+++ app/models/button_se.rb
@@ -10,12 +10,10 @@ class ButtonSe
     :path           => '/button_se/:attachment/:id/:filename',
     :storage        => :s3,
     :url            => ':s3_alias_url',
-    :s3_host_alias  => 'radiheyrails.s3.amazonaws.com',
     :s3_credentials => File.join(Rails.root, 'config', 's3.yml')
   }
 
   validates :name, presence: true
-  validates_attachment :audio, presence: true,
-    content_type: {content_type: ['audio/wav','audio/x-wav']},
-    size: {in: 0..1.megabytes}
+  validates_attachment :audio, presence: true, size: {in: 0..1.megabytes}
+  validates_attachment_content_type :audio, :content_type => /audio\/(x-)?wav/
 end
--- app/models/custom_button_se.rb
+++ app/models/custom_button_se.rb
@@ -1,21 +1,3 @@
 class CustomButtonSe < ButtonSe
-  include Mongoid::Paperclip
-
   belongs_to :owner, class_name: User.to_s
-
-  has_mongoid_attached_file :uploaded_audio, {
-    :path           => '/button_se/:attachment/:id/:filename',
-    :storage        => :s3,
-    :url            => ':s3_alias_url',
-    :s3_host_alias  => 'radiheyrails.s3.amazonaws.com',
-    :s3_credentials => File.join(Rails.root, 'config', 's3.yml')
-  }
-
-  validates_attachment :uploaded_audio, presence: true,
-    content_type: {content_type: ['audio/wav','audio/x-wav']},
-    size: {in: 0..1.megabytes}
-
-  def audio
-    self.uploaded_audio
-  end
 end
